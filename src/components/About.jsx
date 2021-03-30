a37cb96dd6ad56dad64796764f05003638c45f21
Lindsay Mulhollen <lindsaymulhollen@lindsays-imac-2.lan>
2021-03-30 15:30:08 -0500

    fun section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,26 +1,30 @@
 import React from 'react';
 import lindsay from '../img/LittleLinds.png';
 import emojis from 'emojis-list';
+import Fun from '../shared/Fun';
 import Social from '../shared/Social';
 import { Helmet } from "react-helmet";
 
 const About = () => {
   return (
-    <div className="about bodyWidth d-flex justify-content-center mt-5 about-div">
+    <div className="about bodyWidth d-flex justify-content-center mt-5 flex-column">
       <Helmet>
         <title>LM | About</title>
       </Helmet>
-      <img src={lindsay} className="widthBlock-50" alt="Lindsay as a small child lounging in front of a toy computer" />
-      <div className="aboutWidth">
-        <h1 className="mb-4 nunito">It's ya girl {emojis[1244]}</h1>
-        <p>
-          Inspired to get a start in development after encountering interactive museum exhibits, I'm growing my passion for puzzle solving, brainstorming, and outside of the box thinking as an Apprentice Front-end Developer + UX/UI Designer at Nashville Software School. 
-          <br />
-          <br />                        
-          I'm excited to bring my unique background to the development world where I plan to make my mark with innovative thinking and motivation to tackle even the most unthinkable projects.
-        </p>
-        <Social />
+      <div className="d-flex justify-content-center about-div">
+        <img src={lindsay} className="widthBlock-50" alt="Lindsay as a small child lounging in front of a toy computer" />
+        <div className="aboutWidth">
+          <h1 className="mb-4 nunito">It's ya girl {emojis[1244]}</h1>
+          <p>
+            Inspired to get a start in development after encountering interactive museum exhibits, I'm growing my passion for puzzle solving, brainstorming, and outside of the box thinking as an Apprentice Front-end Developer + UX/UI Designer at Nashville Software School. 
+            <br />
+            <br />                        
+            I'm excited to bring my unique background to the development world where I plan to make my mark with innovative thinking and motivation to tackle even the most unthinkable projects.
+          </p>
+          <Social />
+        </div>
       </div>
+      <Fun />
     </div>
   );
 }
