e2e4e26b9cc7b917a2380e319026a6a6bac2cd5c
Lindsay Mulhollen <lindsaymulhollen@lindsays-imac-2.lan>
2021-03-30 14:13:14 -0500

    adding myspace about

--- src/components/parts/Blurbs.jsx
+++ src/components/parts/Blurbs.jsx
@@ -7,9 +7,11 @@ const Blurbs = () => {
       <div className="pl-3">
         <h4 className="mb-0">About Me:</h4>
         <p>
-          Inspired to get a start in development after encountering interactive museum exhibits, I'm growing my passion for puzzle solving, brainstorming, and outside of the box thinking as an Apprentice Front-end Developer + UX/UI Designer at Nashville Software School. 
-          I'm excited to bring my unique background to the development world where I plan to make my mark with innovative thinking and motivation to tackle even the most unthinkable projects.
-        </p>
+          Hello World and Welcome to LindsSpace. I know I said I was inspired to get into dev based on seeing interactive museum exhibits (which is true), but once I was in school a little memory came to me, a touch of deja vu. Let me set the stage, I just got home from ballet rehearsals, I hear the instantly recognizable and enthralling sound of a digital door opening, and the bing-bing-bing, upscale chimes start to roll in: it was time to edit my myspace profile. Now, no hate on glitter gifs, but that wasn’t my scene; I was out there flinging around “visibility: hidden” and moving things around. My myspace was my piece de resistance, my personal brand.
+          <br />
+          <br />
+          Now with the clarity of the future, I realize I was out there writin’ that code even earlier than I realized. This is my homage to my past formative experience and a fun playground for someone with nostalgia for early social networking. Also, being a UX dev, I decided it’d be fun to think how myspace might have looked if it were accessible on mobile. Unfortunately, I have no current plans to add embarrassing emo pictures, but I’m not against it, so shoot me a message if you think that’d be rad addition. x linds
+         </p>
       </div>
     </div>
   );
