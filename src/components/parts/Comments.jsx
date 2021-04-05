be7123045a609ad7c1217e4aa996299e06c2de47
Lindsay Mulhollen <lindsay.mulhollen@healthstream.com>
2021-04-05 14:27:51 -0500

    added comment

--- src/components/parts/Comments.jsx
+++ src/components/parts/Comments.jsx
@@ -18,7 +18,22 @@ const Comments = () => {
         <a href={`mailto:lindsaymulhollen@gmail.com`}> Add Comment </a>
         )</b></p>
       </div>
-      <div className="px-3 d-flex w-100 myspace__blurbs--topdiv justify-content-between">
+      <div className="px-3 d-flex flex-column w-100 myspace__blurbs--topdiv justify-content-between">
+        
+        <div className="d-flex myspace__blurbs--comment mb-1 flip-small">
+          <div className="p-1 mr-1 d-flex flex-column align-items-center">
+            <a href="www.melissarobertson.com" traget="_blank" className="friendLink">
+              <h4>Melissa Robertson,<br/>HealthStream Colleague</h4>
+              <img className="mb-1" src="https://melissarobertson.com/img/Wheatley_Headshot2018.jpg" alt=" " />
+            </a> 
+          </div>
+          <div className="p-1">
+            <p className="font-weight-bold">April 2021</p>
+            <p>
+              Lindsay takes challenges and crushes them, showing an ability to rapidly level up and adapt that is inspiring. As we've worked together, I've come to view her as a trusted resource whether I'm looking for someone to work through code with or a sounding board while thinking through a design. Her leadership capabilities shine when she is given opportunities to share knowledge and her enthusiasm is contagious. She is truly an asset for any team!
+            </p>
+          </div>
+        </div>
         <div className="d-flex myspace__blurbs--comment mb-1 flip-small">
           <div className="p-1 mr-1 d-flex flex-column align-items-center">
             <h4>HealthStream Colleague</h4>
@@ -29,11 +44,11 @@ const Comments = () => {
             <p>Thanks for crossing the aisle and inspiring me to Stream Good. You gave me the most energetic welcome and introduction that I've had since I've been here. By far the best interaction that I've had!</p>
           </div>
         </div>
+
       </div>
     </div>
   );
 }
 
 export default Comments;
-
 
